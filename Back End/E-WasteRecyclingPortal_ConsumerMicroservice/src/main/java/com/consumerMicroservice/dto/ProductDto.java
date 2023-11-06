89ae1e223b36bbaaf9832be20566ad4318a871db
Thiyagarajan.M <81804719+28Thiyagarajan-M@users.noreply.github.com>
2023-11-06 16:03:41 +0530

    Add files via upload

---
+++ Back End/E-WasteRecyclingPortal_ConsumerMicroservice/src/main/java/com/consumerMicroservice/dto/ProductDto.java
@@ -0,0 +1,32 @@
+package com.consumerMicroservice.dto;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int productId;
+	private String name;
+	private String category;
+	private String brand;
+	private String accessories;
+	private int manufacturedYear;
+	private int consumerId;
+	private boolean status;
+}
---
+++ Back End/E-WasteRecyclingPortal_ProductMicroservice/src/main/java/com/productMicroservice/exception/NoContentFoundException.java
@@ -0,0 +1,16 @@
+package com.productMicroservice.exception;
+
+public class NoContentFoundException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+    private final String message;
+
+    public NoContentFoundException() {
+        this.message = "";
+    }
+
+    public NoContentFoundException(String message) {
+        this.message = message;
+    }
+
+}
